10cbcfeb8e0186cf70a57210fffd2861813b674e
Fawaz Salimanu <salimanufawaz@gmail.com>
2024-11-14 19:02:14 +0100

    ,

--- week-2/project_3.rs
+++ week-2/project_3.rs
@@ -4,5 +4,5 @@ fn main() {
     let t: f64 = 3.0;
 
     let a: f64 = p* ((1.0 - (r/100.0)).powf(t));
-    println!("The value of the TV set after 3 years is {a}")
+    println!("The value of the TV set after 3 years is {a}");
 }
